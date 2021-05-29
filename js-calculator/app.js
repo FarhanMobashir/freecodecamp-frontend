158aaa10c2357e86d62e40f636f91793692190fc
Mobashir Farhan <farhanmobashir007@gmail.com>
2021-05-29 12:40:17 +0530

    adds some basic functionalities in calculator

--- js-calculator/app.js
+++ js-calculator/app.js
@@ -1,20 +1,57 @@
-// result
+// ? Elemets
+const MinDisplay = document.querySelector(".min-display");
+const answer = document.querySelector(".answer");
+const buttons = document.querySelectorAll(".buttons");
+console.log(MinDisplay, buttons, answer);
 
+// ? Event Listeners
+buttons.forEach((btn) => btn.addEventListener("click", main));
+
+// ? Functions
 let result = "";
-let count = 0;
-let output = result;
-// elements
-let clear = document.querySelector("#clear");
-let preview = document.querySelector(".min-display");
-let display = document.querySelector(".answer");
+function updateMinDisplay(e) {
+  if (result.length < 30) {
+    if (
+      e.target.innerText !== "AC" &&
+      e.target.innerText !== "." &&
+      e.target.innerText !== "="
+    ) {
+      result += e.target.innerText;
+      console.log(result);
+      MinDisplay.innerText = result;
+    }
+  }
+}
 
-let button = document.querySelectorAll("button").forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    setTimeout(() => {
-      e.target.style.background = "cadetblue";
-    }, 100);
-    e.target.style.background = "blue";
-  });
-});
+function clear(e) {
+  if (e.target.innerText === "AC") {
+    result = "";
+    MinDisplay.innerText = "=>";
+    answer.innerText = result;
+  }
+}
 
-// console.log(result);
+function getResult(e) {
+  if (e.target.innerText === "=") {
+    let mainResult = eval(result);
+    answer.innerText = mainResult;
+    result = "";
+  }
+}
+
+function restartAfterEqualCLick(e) {
+  if (answer.innertext !== "") {
+    result = "";
+    answer.innerText = result;
+  }
+}
+
+function main(e) {
+  setTimeout(() => {
+    e.target.style.background = "cadetblue";
+  }, 100);
+  e.target.style.background = "dodgerblue";
+  updateMinDisplay(e);
+  clear(e);
+  getResult(e);
+}
