733b9d69ddefdee379c4ece1cd0ccb503a06ae31
Mobashir Farhan <farhanmobashir007@gmail.com>
2021-06-20 21:13:05 +0530

    fixes css in timer

--- js-calculator/app.js
+++ js-calculator/app.js
@@ -9,10 +9,52 @@ buttons.forEach((btn) => btn.addEventListener("click", main));
 
 // ? Functions
 let result = "";
-function updateMinDisplay(e) {
+let mainResult = "";
+
+function isOperator(n) {
+  let operator = ["+", "-", "/", "*"];
+  let set = new Set(operator);
+  // console.log(set);
+  if (set.has(n)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// update the mini diplay for operators
+function updateMinDisplayForOperators(e) {
+  // let curr = e.target.innerText;
+  if (result.length < 30) {
+    if (
+      isOperator(result[result.length - 1]) !== false &&
+      result.length !== 0
+    ) {
+      if (
+        e.target.innerText === "+" ||
+        e.target.innerText === "-" ||
+        e.target.innerText === "*" ||
+        e.target.innerText === "/"
+      ) {
+        result += e.target.innerText;
+        console.log(result);
+        console.log(isOperator(result[result.length - 1]));
+
+        MinDisplay.innerText = result;
+      }
+    }
+  }
+}
+
+// update the mini diplay for numbers
+function updateMinDisplayForNumber(e) {
   if (result.length < 30) {
     if (
       e.target.innerText !== "AC" &&
+      e.target.innerText !== "*" &&
+      e.target.innerText !== "+" &&
+      e.target.innerText !== "-" &&
+      e.target.innerText !== "/" &&
       e.target.innerText !== "." &&
       e.target.innerText !== "="
     ) {
@@ -23,22 +65,24 @@ function updateMinDisplay(e) {
   }
 }
 
+// on clicking AC it reset the results
 function clear(e) {
   if (e.target.innerText === "AC") {
     result = "";
     MinDisplay.innerText = "=>";
-    answer.innerText = result;
+    answer.innerText = "";
   }
 }
 
+// on clicking equal it displays the result on display
 function getResult(e) {
   if (e.target.innerText === "=") {
-    let mainResult = eval(result);
+    mainResult = eval(result);
     answer.innerText = mainResult;
-    result = "";
   }
 }
 
+// works after first click
 function restartAfterEqualCLick(e) {
   if (answer.innertext !== "") {
     result = "";
@@ -46,12 +90,19 @@ function restartAfterEqualCLick(e) {
   }
 }
 
+// main function
 function main(e) {
   setTimeout(() => {
     e.target.style.background = "cadetblue";
   }, 100);
   e.target.style.background = "dodgerblue";
-  updateMinDisplay(e);
   clear(e);
+  updateMinDisplayForNumber(e);
+  updateMinDisplayForOperators(e);
+  if (mainResult !== "") {
+    clear(e);
+    console.log("Hello");
+  }
+
   getResult(e);
 }
